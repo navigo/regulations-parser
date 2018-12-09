66eea5131c7f301824655b41478d20d426961f02
eadamsatx <adamse@gmail.com>
2018-12-09 12:13:46 -0600

    Fixed a merge error

--- regparser/web/settings/parser.py
+++ regparser/web/settings/parser.py
@@ -106,21 +106,20 @@ CUSTOM_CITATIONS = {
     "ATF I 5300.1": "https://atf-eregs.apps.cloud.gov/static/atf_eregs/5300_1.pdf",
     "ATF I 5300.2": "https://www.atf.gov/file/58806/download"}
 
-<<<<<<< HEAD:settings.py
-PREPROCESSORS = plugins.extend_list('eregs_ns.parser.preprocessors', [
-    "regparser.tree.xml_parser.preprocessors.MoveLastAMDPar",
-    "regparser.tree.xml_parser.preprocessors.SupplementAMDPar",
-    "regparser.tree.xml_parser.preprocessors.ParenthesesCleanup",
-    "regparser.tree.xml_parser.preprocessors.MoveAdjoiningChars",
-    "regparser.tree.xml_parser.preprocessors.ApprovalsFP",
-    "regparser.tree.xml_parser.preprocessors.ExtractTags",
-    "regparser.tree.xml_parser.preprocessors.Footnotes",
-    "regparser.tree.xml_parser.preprocessors.ParseAMDPARs",
-    "regparser.tree.xml_parser.preprocessors.AtfI50032",
-    "regparser.tree.xml_parser.preprocessors.AtfI50031",
-    "regparser.tree.xml_parser.preprocessors.ImportCategories",
-    "regparser.tree.xml_parser.preprocessors.MoveSubpart",
-])
+# PREPROCESSORS = plugins.extend_list('eregs_ns.parser.preprocessors', [
+#     "regparser.tree.xml_parser.preprocessors.MoveLastAMDPar",
+#     "regparser.tree.xml_parser.preprocessors.SupplementAMDPar",
+#     "regparser.tree.xml_parser.preprocessors.ParenthesesCleanup",
+#     "regparser.tree.xml_parser.preprocessors.MoveAdjoiningChars",
+#     "regparser.tree.xml_parser.preprocessors.ApprovalsFP",
+#     "regparser.tree.xml_parser.preprocessors.ExtractTags",
+#     "regparser.tree.xml_parser.preprocessors.Footnotes",
+#     "regparser.tree.xml_parser.preprocessors.ParseAMDPARs",
+#     "regparser.tree.xml_parser.preprocessors.AtfI50032",
+#     "regparser.tree.xml_parser.preprocessors.AtfI50031",
+#     "regparser.tree.xml_parser.preprocessors.ImportCategories",
+#     "regparser.tree.xml_parser.preprocessors.MoveSubpart",
+# ])
 
 ## Which layers are to be generated, keyed by document type. The ALL key is
 ## special; layers in this category automatically apply to all document types
